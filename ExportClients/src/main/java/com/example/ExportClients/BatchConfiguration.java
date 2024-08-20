20541ef8e2c116e2f98d89905391e86dc2a480d8
amparo <ampiguisamonde@gmail.com>
2024-08-20 15:00:23 -0300

    fix batchConfiguration

--- ExportClients/src/main/java/com/example/ExportClients/BatchConfiguration.java
+++ ExportClients/src/main/java/com/example/ExportClients/BatchConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.file.FlatFileHeaderCallback;
 import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
 import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.WritableResource;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.batch.item.ExecutionContext;
+
 
 import com.example.ExportClients.model.Cliente;
 import com.example.ExportClients.model.ClienteRowMapper;
@@ -34,71 +39,81 @@ public class BatchConfiguration {
 
 	@Autowired
 	public PlatformTransactionManager transactionManager;
-
+	
 	@Autowired
 	public DataSource dataSource;
 
 	@Autowired
 	public Environment environment;
 
+//	@Bean
+//	public JdbcCursorItemReader<Cliente> reader() throws Exception {
+//
+//		JdbcCursorItemReader itemReader = new JdbcCursorItemReader();
+//		itemReader.setDataSource(dataSource);
+//		itemReader.setSql( "SELECT id, nombre, apellido, email, telefono FROM clientes");
+//		itemReader.setRowMapper(new ClienteRowMapper());
+//		int counter = 0;
+//		ExecutionContext executionContext = new ExecutionContext();
+//		itemReader.open(executionContext);
+//		Object cliente = new Object();
+//		while(cliente != null){
+//		    cliente = itemReader.read();
+//		    counter++;
+//		}
+//		
+//		return itemReader;
+//	}
+	
 	@Bean
 	public JdbcCursorItemReader<Cliente> reader() {
-
-		JdbcCursorItemReader<Cliente> reader = new JdbcCursorItemReader<>();
-
-		reader.setDataSource(dataSource);
-
-		String sql = "SELECT id, nombre, apellido, email, telefono FROM clientes";
-
-		reader.setSql(sql);
-
-		reader.setRowMapper(new ClienteRowMapper());
-		return reader;
+	    JdbcCursorItemReader itemReader = new JdbcCursorItemReader();
+	    itemReader.setDataSource(dataSource);
+	    itemReader.setSql("SELECT id, nombre, apellido, email, telefono FROM clientes");
+	    itemReader.setRowMapper(new ClienteRowMapper());
+	    
+	    System.out.println("item reader" + itemReader);
+	    
+	    return itemReader;
 	}
 
+//	@Bean
+//	public ClienteItemProcessor processor() {
+//		return new ClienteItemProcessor();
+//	}
+	
 	@Bean
-	public ClienteItemProcessor processor() {
-		return new ClienteItemProcessor();
+	public FlatFileItemWriter<Cliente> itemWriter() throws Exception {
+		BeanWrapperFieldExtractor<Cliente> fieldExtractor = new BeanWrapperFieldExtractor<>();
+		String decodedpath = environment.getProperty("file.output.path");
+		fieldExtractor.setNames(new String[] {"id", "nombre", "apellido","email", "telefono"});
+		fieldExtractor.afterPropertiesSet();
+
+		DelimitedLineAggregator<Cliente> lineAggregator = new DelimitedLineAggregator<>();
+		lineAggregator.setDelimiter(",");
+		lineAggregator.setFieldExtractor(fieldExtractor);
+		FileSystemResource outputResource = new FileSystemResource(decodedpath + "clientes.csv");
+
+		return new FlatFileItemWriterBuilder<Cliente>()
+					.name("clienteWriter")
+					.resource(outputResource)
+					.lineAggregator(lineAggregator)
+					.build();
 	}
 
-	@Bean
-	public FlatFileItemWriter<Cliente> writer() {
-
-		FlatFileItemWriter<Cliente> writer = new FlatFileItemWriter<>();
-
-		String outputPath = environment.getProperty("file.output.path");
-		writer.setResource(new FileSystemResource(outputPath + "clientes.csv"));
-
-		writer.setHeaderCallback(new FlatFileHeaderCallback() {
-			public void writeHeader(Writer writer) throws IOException {
-
-				writer.write("id,nombre,apellido,email,telefono");
-			}
-		});
-
-		writer.setLineAggregator(new DelimitedLineAggregator<Cliente>() {
-			{
-				setDelimiter(",");
-				setFieldExtractor(new BeanWrapperFieldExtractor<Cliente>() {
-					{
-						setNames(new String[] { "id", "nombre", "apellido", "email", "telefono" });
-					}
-				});
-			}
-		});
-		return writer;
-	}
 
 	@Bean
-	public Step step1() {
+	public Step step1() throws Exception {
 		StepBuilder stepBuilder = new StepBuilder("step1", jobRepository);
-		return stepBuilder.<Cliente, Cliente>chunk(10, transactionManager).reader(reader()).processor(processor())
-				.writer(writer()).build();
+		return stepBuilder.<Cliente, Cliente>chunk(10, transactionManager)
+				.reader(reader())
+				.writer(itemWriter()).build();
 	}
 
 	@Bean
-	public Job exportClienteJob() {
-		return new JobBuilder("exportClienteJob", jobRepository).incrementer(new RunIdIncrementer()).start(step1())
+	public Job exportClienteJob() throws Exception {
+		return new JobBuilder("exportClienteJob", jobRepository).incrementer(new RunIdIncrementer())
+				.start(step1())
 				.build();
 	}
 }
--- ExportClients/src/main/java/com/example/ExportClients/ClienteItemProcessor.java
+++ ExportClients/src/main/java/com/example/ExportClients/ClienteItemProcessor.java
@@ -9,6 +9,7 @@ public class ClienteItemProcessor implements ItemProcessor<Cliente, Cliente>  {
 	@Override
 	public Cliente process(Cliente item) throws Exception {
 		// TODO Auto-generated method stub
+		System.out.println("sysout en la clase clienteitemprocessor, item: " + item);
 		return item;
 	}
 
